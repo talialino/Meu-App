1df0453cbdcf6218d88a42da03e0be9ed0671b26
talialino <talia-apiu@hotmail.com>
2020-09-21 23:18:49 -0300

    adding screen

--- app/src/main/java/com/piu/doces_app/MainActivity.kt
+++ app/src/main/java/com/piu/doces_app/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.piu.doces_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bt1.setOnClickListener {
+            val intent = Intent(this, cupomActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
---
+++ app/src/main/java/com/piu/doces_app/cupomActivity.kt
@@ -0,0 +1,11 @@
+package com.piu.doces_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class cupomActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cupom)
+    }
+}
