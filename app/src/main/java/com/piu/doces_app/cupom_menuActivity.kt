ee930ae8f3b8a36d89cfb55b53f1158a597f220e
talialino <talia-apiu@hotmail.com>
2020-09-22 21:17:25 -0300

    deleting bugs

--- app/src/main/java/com/piu/doces_app/cupom_menuActivity.kt
+++ app/src/main/java/com/piu/doces_app/cupom_menuActivity.kt
@@ -3,12 +3,9 @@ package com.piu.doces_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class cupomActivity : AppCompatActivity() {
+class cupom_menuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cupom)
-
-        var d = 0
-        d++
+        setContentView(R.layout.activity_cupom_menu)
     }
 }
